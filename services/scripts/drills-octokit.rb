b02e599c9f5078b44f28b246317eb8a39532fb01
Bruce Becker <brucellino@gmail.com>
2017-01-29 11:23:00 +0100

    Update Octokit drills version with working issues
    The drills script has been checked and is working up until the
    topic post part. The issues are correctly created, and we have added
    a few templates for these. There is still no checking to see whether
    the issues are already created in the milestone or drill project,
    but at least the formatting is correct.
    
    The parts relative to the topic creation on discourses is commented
    out still, since it's not yet working.
    
    Closed #40
    Closed #41
    Closed #42
    Closed #43
    Closed #44
    Closed #45
    Closed #46
    Closed #47
    Closed #48
    Closed #49
    Closed #50
    Closed #51
    Closed #52
    Closed #53
    Closed #54
    Closed #55
    Closed #56
    Closed #57
    Closed #58
    Closed #59
    Closed #60

--- services/scripts/drills-octokit.rb
+++ services/scripts/drills-octokit.rb
@@ -7,12 +7,13 @@ require "octokit"
 
 # Use HTTParty to interact with other REST APIs
 require "httparty"
+# This is fir discourse :
+discourse_api_url = "http://discourse.sci-gaia.eu"
+discourse_api_key = ENV["discourse_api_key"]
 
 octobot = Octokit::Client.new(:login => 'brucellino', :access_token => ENV['github_token'])
-
 github_org = "AAROC"
 github_repo = "e-Research-Hackfest-Prep"
-
 # Use the Github API naming schema for the variables
 milestone_title = "Aspring to Addis"
 project_name = "Hackfest Drills"
@@ -46,7 +47,27 @@ milestone = milestones.select {|m| m["title"] == "Aspiring to Addis" } # somethi
 
 
 ## Create the issues.
-file = File.read('drill_issues.json')
-issue_list = JSON.parse(file)
+
+issue_list = JSON.parse(File.read('drill_issues.json'))
 ap issue_list
 puts "We have to create #{issue_list["issues"].count} issues to create"
+
+issue_list["issues"].each do |issue|
+  puts "creating issue #{issue["id"]}"
+  puts issue["name"]
+  issue_content = JSON.parse(File.read("../static-files/issue_templates/#{issue["template_file"]}"))
+  ap issue_content
+  #issue_body = octobot.markdown('#{issue_content}')
+  #octobot.create_issue("#{github_org}/#{github_repo}",issue_content["title"],issue_content["body"])
+end
+
+puts "creating the topic"
+
+post = JSON.parse(File.read("../static-files/post_templates/readiness_post.json"))
+ap post
+
+discourse_post_uri = discourse_api_url + "/posts"
+#options = { :api_key => discourse_api_key, :api_user => 'system', }
+#response = HTTParty.post(discourse_post_uri, body: post, query: options)
+
+#ap response
