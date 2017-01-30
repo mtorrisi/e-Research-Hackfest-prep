31a7729234f091c69b8bc1becfa1563819a2fd06
Bruce Becker <brucellino@gmail.com>
2017-01-30 06:48:52 +0100

    Add checks on the discussion topic existence

--- services/scripts/drills-octokit.rb
+++ services/scripts/drills-octokit.rb
@@ -2,14 +2,43 @@
 # Use Awesome Print for JSON output
 require "awesome_print"
 require "json"
+require  "date"
 # Use octokit for interacting with GitHub
 require "octokit"
 
 # Use HTTParty to interact with other REST APIs
 require "httparty"
-# This is fir discourse :
+# This is for discourse :
 discourse_api_url = "http://discourse.sci-gaia.eu"
 discourse_api_key = ENV["discourse_api_key"]
+post = JSON.parse(File.read("../static-files/post_templates/readiness_post.json"))
+# Construct the title
+post["title"] = "#{post["title"]}-"
+# Get categories
+
+categories = HTTParty.get("#{discourse_api_url}/categories.json")
+sandbox_category = categories["category_list"]["categories"].select{ |c| c["slug"] == "sandbox"}
+sandbox_category_id = sandbox_category[0]["id"]
+
+# Get the list of topics in that category
+topics = HTTParty.get("#{discourse_api_url}/c/#{sandbox_category_id}.json")
+# is there a topic with this title ?
+if topics["topic_list"]["topics"].any? { |t| t["title"] ==  }
+
+
+  ap post
+  # Update post title for
+
+  discourse_post_uri = discourse_api_url + "/posts"
+  options = { :api_key => discourse_api_key, :api_username => 'system', :category => "sandbox"}
+  response = HTTParty.post(discourse_post_uri, body: post, query: options)
+
+  # check if the title has already been created - if so, update the topic with a new post
+  if response["errors"].to_s.match('Title has already been used')
+    puts "the title has already been taken, finding that one"
+  end
+
+
 
 octobot = Octokit::Client.new(:login => 'brucellino', :access_token => ENV['github_token'])
 github_org = "AAROC"
@@ -18,7 +47,6 @@ github_repo = "e-Research-Hackfest-Prep"
 milestone_title = "Aspring to Addis"
 project_name = "Hackfest Drills"
 issue_labels = ["todo", "checking", "missing", "done"]
-
 
 ############# milestones ###############################
 milestones = octobot.list_milestones("#{github_org}/#{github_repo}")
@@ -63,11 +91,5 @@ end
 
 puts "creating the topic"
 
-post = JSON.parse(File.read("../static-files/post_templates/readiness_post.json"))
-ap post
-
-discourse_post_uri = discourse_api_url + "/posts"
-#options = { :api_key => discourse_api_key, :api_user => 'system', }
-#response = HTTParty.post(discourse_post_uri, body: post, query: options)
 
 #ap response
